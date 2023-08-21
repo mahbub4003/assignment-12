b96c01d9c36bb70e1dc7673478454750228b7c0d
mahbub-alam <mahbub01750934003@gmail.com>
2023-08-22 00:20:19 +0600

    fits2

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // distDir: "build",
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "roar.media",
-  //     },
-  //   ],
-  // },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/:path*",
-  //       headers: [{ key: "X-Frame-Options", value: "DENY" }],
-  //     },
-  //   ];
-  // },
+  distDir: "build",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "roar.media",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [{ key: "X-Frame-Options", value: "DENY" }],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
